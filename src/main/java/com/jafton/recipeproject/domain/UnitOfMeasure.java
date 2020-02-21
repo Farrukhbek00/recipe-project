25ba1d7ce9fc4dc47075c498741c5426deb602e1
Farrukhbek <farruhzokirov00@gmail.com>
2020-02-21 16:52:38 +0500

    Enumaration added

--- src/main/java/com/jafton/recipeproject/domain/UnitOfMeasure.java
+++ src/main/java/com/jafton/recipeproject/domain/UnitOfMeasure.java
@@ -1,4 +1,30 @@
 package com.jafton.recipeproject.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String uom;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUom() {
+        return uom;
+    }
+
+    public void setUom(String uom) {
+        this.uom = uom;
+    }
+
 }
